b9a03d2fd62d5745dc7c77869fbee56289e9733c
Julian Reichen <121826818+HopperLogger@users.noreply.github.com>
2025-08-28 10:15:05 +0200

    Added target fps and custom res scalar setting

--- HopperRender/HopperRenderSettings.cpp
+++ HopperRender/HopperRenderSettings.cpp
@@ -71,8 +71,8 @@ INT_PTR CHopperRenderSettings::OnReceiveMessage(HWND hwnd, UINT uMsg, WPARAM wPa
 			int frameOutput;
 			int activeState;
 			double currentFPS = m_dSourceFPS;
-			m_pSettingsInterface->GetCurrentSettings(&m_bActivated, &frameOutput, &m_iDeltaScalar, &m_iNeighborScalar, &m_iBlackLevel, &m_iWhiteLevel,
-													 &activeState, &m_dSourceFPS, &m_dTargetFPS, &m_dOFCCalcTime, &m_dWarpCalcTime, &iDimX, &iDimY, &iLowDimX, &iLowDimY);
+			m_pSettingsInterface->GetCurrentSettings(&m_bActivated, &frameOutput, &m_dTargetFPS, &m_iDeltaScalar, &m_iNeighborScalar, &m_iBlackLevel, &m_iWhiteLevel,
+													 &activeState, &m_dSourceFPS, &m_dOFCCalcTime, &m_dWarpCalcTime, &iDimX, &iDimY, &iLowDimX, &iLowDimY);
 			m_iFrameOutput = static_cast<FrameOutput>(frameOutput);
 			m_iIntActiveState = static_cast<ActiveState>(activeState);
 
@@ -103,10 +103,6 @@ INT_PTR CHopperRenderSettings::OnReceiveMessage(HWND hwnd, UINT uMsg, WPARAM wPa
 			TCHAR sz[60];
 			(void)StringCchPrintf(sz, NUMELMS(sz), TEXT("%.3f fps\0"), m_dSourceFPS);
 			SetDlgItemText(m_Dlg, IDC_SOURCEFPS, sz);
-
-			// Update the target frames per second
-			(void)StringCchPrintf(sz, NUMELMS(sz), TEXT("%.3f fps\0"), m_dTargetFPS);
-			SetDlgItemText(m_Dlg, IDC_TARGETFPS, sz);
 
 			// Update the OFC Calc Time
 			(void)StringCchPrintf(sz, NUMELMS(sz), TEXT("%.3f ms\0"), m_dOFCCalcTime);
@@ -147,8 +143,8 @@ HRESULT CHopperRenderSettings::OnConnect(IUnknown* pUnknown) {
 	int frameOutput;
 	int activeState;
 	CheckPointer(m_pSettingsInterface, E_FAIL);
-	m_pSettingsInterface->GetCurrentSettings(&m_bActivated, &frameOutput, &m_iDeltaScalar, &m_iNeighborScalar, &m_iBlackLevel, &m_iWhiteLevel,
-	                                         &activeState, &m_dSourceFPS, &m_dTargetFPS, &m_dOFCCalcTime, &m_dWarpCalcTime, &iDimX, &iDimY, &iLowDimX, &iLowDimY);
+	m_pSettingsInterface->GetCurrentSettings(&m_bActivated, &frameOutput, &m_dTargetFPS, &m_iDeltaScalar, &m_iNeighborScalar, &m_iBlackLevel, &m_iWhiteLevel,
+	                                         &activeState, &m_dSourceFPS, &m_dOFCCalcTime, &m_dWarpCalcTime, &iDimX, &iDimY, &iLowDimX, &iLowDimY);
 	m_iFrameOutput = static_cast<FrameOutput>(frameOutput);
 	m_iIntActiveState = static_cast<ActiveState>(activeState);
 
@@ -201,6 +197,10 @@ HRESULT CHopperRenderSettings::OnActivate() {
 			CheckRadioButton(m_Dlg, IDC_WARPEDFRAME12, IDC_SIDEBYSIDE2, IDC_SIDEBYSIDE2);
 			break;
 	}
+
+	// Set the initial Target FPS
+	(void)StringCchPrintf(sz, NUMELMS(sz), TEXT("%.3f\0"), m_dTargetFPS);
+	SetDlgItemText(m_Dlg, IDC_TARGETFPS, sz);
 
 	// Set the initial Delta Scalar
 	(void)StringCchPrintf(sz, NUMELMS(sz), TEXT("%d\0"), m_iDeltaScalar);
@@ -260,7 +260,7 @@ HRESULT CHopperRenderSettings::OnApplyChanges() {
 	ValidateParameter(m_iWhiteLevel, 65535, IDC_WHITELEVEL);
 
 	// Tell the filter about the new settings
-	m_pSettingsInterface->UpdateUserSettings(m_bActivated, m_iFrameOutput, m_iDeltaScalar, m_iNeighborScalar, m_iBlackLevel, m_iWhiteLevel);
+	m_pSettingsInterface->UpdateUserSettings(m_bActivated, m_iFrameOutput, m_dTargetFPS, m_iDeltaScalar, m_iNeighborScalar, m_iBlackLevel, m_iWhiteLevel);
 
 	// Save the settings to the registry
 	if (saveSettings() != S_OK) {
@@ -273,7 +273,7 @@ HRESULT CHopperRenderSettings::OnApplyChanges() {
 // Get the values from the controls
 void CHopperRenderSettings::GetControlValues() {
 	TCHAR sz[STR_MAX_LENGTH];
-	int tmp1, tmp2, tmp3, tmp4;
+	int tmp1, tmp2, tmp3, tmp4, tmp5;
 
 	// Find whether the filter is activated or not
 	if (IsDlgButtonChecked(m_Dlg, IDC_ON)) {
@@ -299,20 +299,21 @@ void CHopperRenderSettings::GetControlValues() {
 		m_iFrameOutput = SideBySide2;
 	}
 
-	// Get the delta scalar value
-	Edit_GetText(GetDlgItem(m_Dlg, IDC_DELTASCALAR), sz, STR_MAX_LENGTH);
+	// Get the target fps
+	Edit_GetText(GetDlgItem(m_Dlg, IDC_TARGETFPS), sz, STR_MAX_LENGTH);
 
 #ifdef UNICODE
 	// Convert Multibyte string to ANSI
 	char szANSI[STR_MAX_LENGTH];
-	int rc = WideCharToMultiByte(CP_ACP, 0, sz, -1, szANSI, STR_MAX_LENGTH, nullptr, nullptr);
-	tmp1 = atoi(szANSI);
+	int rc = WideCharToMultiByte(CP_ACP, 0, sz, -1, szANSI, STR_MAX_LENGTH,
+				     nullptr, nullptr);
+	tmp1 = atof(szANSI);
 #else
-    tmp1 = atoi(sz);
+	tmp1 = atof(sz);
 #endif
 
-	// Get the neighbor scalar value
-	Edit_GetText(GetDlgItem(m_Dlg, IDC_NEIGHBORSCALAR), sz, STR_MAX_LENGTH);
+	// Get the delta scalar value
+	Edit_GetText(GetDlgItem(m_Dlg, IDC_DELTASCALAR), sz, STR_MAX_LENGTH);
 
 #ifdef UNICODE
 	// Convert Multibyte string to ANSI
@@ -323,19 +324,20 @@ void CHopperRenderSettings::GetControlValues() {
     tmp2 = atoi(sz);
 #endif
 
-	// Get the black level
-	Edit_GetText(GetDlgItem(m_Dlg, IDC_BLACKLEVEL), sz, STR_MAX_LENGTH);
+	// Get the neighbor scalar value
+	Edit_GetText(GetDlgItem(m_Dlg, IDC_NEIGHBORSCALAR), sz, STR_MAX_LENGTH);
 
 #ifdef UNICODE
 	// Convert Multibyte string to ANSI
+	szANSI[STR_MAX_LENGTH];
 	rc = WideCharToMultiByte(CP_ACP, 0, sz, -1, szANSI, STR_MAX_LENGTH, nullptr, nullptr);
 	tmp3 = atoi(szANSI);
 #else
     tmp3 = atoi(sz);
 #endif
 
-	// Get the white level
-	Edit_GetText(GetDlgItem(m_Dlg, IDC_WHITELEVEL), sz, STR_MAX_LENGTH);
+	// Get the black level
+	Edit_GetText(GetDlgItem(m_Dlg, IDC_BLACKLEVEL), sz, STR_MAX_LENGTH);
 
 #ifdef UNICODE
 	// Convert Multibyte string to ANSI
@@ -345,10 +347,22 @@ void CHopperRenderSettings::GetControlValues() {
     tmp4 = atoi(sz);
 #endif
 
-	m_iDeltaScalar = tmp1;
-	m_iNeighborScalar = tmp2;
-	m_iBlackLevel = tmp3;
-	m_iWhiteLevel = tmp4;
+	// Get the white level
+	Edit_GetText(GetDlgItem(m_Dlg, IDC_WHITELEVEL), sz, STR_MAX_LENGTH);
+
+#ifdef UNICODE
+	// Convert Multibyte string to ANSI
+	rc = WideCharToMultiByte(CP_ACP, 0, sz, -1, szANSI, STR_MAX_LENGTH, nullptr, nullptr);
+	tmp5 = atoi(szANSI);
+#else
+    tmp5 = atoi(sz);
+#endif
+
+	m_dTargetFPS = tmp1;
+	m_iDeltaScalar = tmp2;
+	m_iNeighborScalar = tmp3;
+	m_iBlackLevel = tmp4;
+	m_iWhiteLevel = tmp5;
 }
 
 // Saves the settings to the registry
@@ -366,22 +380,25 @@ HRESULT CHopperRenderSettings::saveSettings() {
 		// Save Frame Output
 		LONG result2 = RegSetValueEx(hKey, L"FrameOutput", 0, REG_DWORD, reinterpret_cast<BYTE*>(&m_iFrameOutput), sizeof(DWORD));
 
+		// Save the target fps
+		LONG result3 = RegSetValueEx(hKey, L"TargetFPS", 0, REG_BINARY, reinterpret_cast<const BYTE*>(&m_dTargetFPS), sizeof(double));
+
 		// Save the delta scalar
-		LONG result3 = RegSetValueEx(hKey, L"DeltaScalar", 0, REG_DWORD, reinterpret_cast<BYTE*>(&m_iDeltaScalar), sizeof(DWORD));
+		LONG result4 = RegSetValueEx(hKey, L"DeltaScalar", 0, REG_DWORD, reinterpret_cast<BYTE*>(&m_iDeltaScalar), sizeof(DWORD));
 
 		// Save the neighbor scalar
-		LONG result4 = RegSetValueEx(hKey, L"NeighborScalar", 0, REG_DWORD, reinterpret_cast<BYTE*>(&m_iNeighborScalar), sizeof(DWORD));
+		LONG result5 = RegSetValueEx(hKey, L"NeighborScalar", 0, REG_DWORD, reinterpret_cast<BYTE*>(&m_iNeighborScalar), sizeof(DWORD));
 
 		// Save the black level
-		LONG result5 = RegSetValueEx(hKey, L"BlackLevel", 0, REG_DWORD, reinterpret_cast<BYTE*>(&m_iBlackLevel), sizeof(DWORD));
+		LONG result6 = RegSetValueEx(hKey, L"BlackLevel", 0, REG_DWORD, reinterpret_cast<BYTE*>(&m_iBlackLevel), sizeof(DWORD));
 
 		// Save the white level
-		LONG result6 = RegSetValueEx(hKey, L"WhiteLevel", 0, REG_DWORD, reinterpret_cast<BYTE*>(&m_iWhiteLevel), sizeof(DWORD));
+		LONG result7 = RegSetValueEx(hKey, L"WhiteLevel", 0, REG_DWORD, reinterpret_cast<BYTE*>(&m_iWhiteLevel), sizeof(DWORD));
 		
 		RegCloseKey(hKey); // Close the registry key
 
 		// Check for errors
-        if (result1 || result2 || result3 || result4 || result5 || result6) {
+        if (result1 || result2 || result3 || result4 || result5 || result6 || result7) {
 			return E_FAIL;
         } else {
             return S_OK;
