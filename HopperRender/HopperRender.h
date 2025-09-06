0e7b9865a3002e453ad95c956d19cdc049cc261b
Julian Reichen <121826818+HopperLogger@users.noreply.github.com>
2025-09-06 16:45:48 +0200

    Added Dolby Vision metadata passthrough

--- HopperRender/HopperRender.h
+++ HopperRender/HopperRender.h
@@ -1,12 +1,9 @@
 #pragma once
 
 #include <chrono>
+#include "iez.h"
 
 #include "opticalFlowCalc.h"
-
-#define TEST_MODE 0 // Enables or disables automatic settings change to allow accurate performance testing (0: Disabled, 1: Enabled)
-#define FT_TARGET 166667 // The target frame rate in 100ns units (e.g., 166667 for 60 fps)
-#define LOG_PERFORMANCE 0 // Whether or not to print debug messages regarding calculation performance (0: Disabled, 1: Enabled)
 
 typedef enum FrameOutput {
     WarpedFrame12,
@@ -30,6 +27,8 @@ class CHopperRender : public CTransformFilter,
                       public ISpecifyPropertyPages,
                       public CPersistStream {
 public:
+    friend class CCustomInputPin;
+
 	DECLARE_IUNKNOWN;
 	static CUnknown* WINAPI CreateInstance(LPUNKNOWN punk, HRESULT* phr);
 
@@ -89,44 +88,27 @@ public:
 
 	// Settings
 	FrameOutput m_iFrameOutput; // What frame output to use (0: WarpedFrame 1 -> 2, 1: WarpedFrame 2 -> 1, 2: BlendedFrame, 3: HSV Flow, 4: Blurred Frames, 5: Side-by-side 1, 6: Side-by-side 2)
-	int m_iNumIterations; // Number of iterations to use in the optical flow calculation (0: As many as possible)
-	int m_iFrameBlurKernelSize; // The size of the blur kernel used to blur the source frames before calculating the optical flow
-	int m_iFlowBlurKernelSize; // The size of the blur kernel used to blur the offset calculated by the optical flow
 	
 	// Video info
 	unsigned int m_iDimX; // The width of the frame
 	unsigned int m_iDimY; // The height of the frame
-	bool m_bInterlaced; // Whether the video is interlaced or not
-	bool m_bHDR; // Whether the video has HDR (BT.2020) or not
 
 	// Timings
 	REFERENCE_TIME m_rtCurrStartTime; // The start time of the current interpolated frame
 	REFERENCE_TIME m_rtSourceFrameTime; // The average frame time of the source frames (at 1.0x speed)
-	REFERENCE_TIME m_rtTargetFrameTime;
+	REFERENCE_TIME m_rtTargetFrameTime; // The frame time that will be interpolated to
 	REFERENCE_TIME m_rtCurrPlaybackFrameTime; // The current frame time of the source frames (accounting for the current playback speed)
-	std::chrono::time_point<std::chrono::high_resolution_clock> m_tpCurrCalcStart; // The start time of the current calculation
-	std::chrono::time_point<std::chrono::high_resolution_clock> m_tpCurrCalcEnd; // The end time of the current calculation
-	double m_dCurrCalcDuration; // The duration of the current calculation
 
 	// Optical flow calculation
 	OpticalFlowCalc* m_pofcOpticalFlowCalc; // Optical flow calculator
-	unsigned char m_cResolutionStep; // Determines which predefined resolution scalar will be used for the optical flow calculation
-	float m_fResolutionDividers[6]; // The predefined resolution dividers
-	float m_fResolutionScalar; // The scalar to scale the resolution with (only used in the optical flow calculation)
-	float m_fResolutionDivider; // The divider to scale the resolution with (only used in the optical flow calculation)
 
 	// Frame output
-	double m_dDimScalar; // The scalar to scale the frame dimensions with depending on the renderer used
 	unsigned int m_iFrameCounter; // Frame counter (relative! i.e. number of source frames received so far)
 	int m_iNumIntFrames; // Number of output samples (interpolated frames) for every input sample (source frame)
-	long m_lBufferRequest; // The number of buffers to use in the output buffer queue
 
 	// Performance and activation status
-	unsigned char m_cNumTimesTooSlow; // The number of times the interpolation has been too slow
 	ActiveState m_iIntActiveState; // The state of the filter (0: Deactivated, 1: Not Needed, 2: Active, 3: Too Slow)
-	bool m_bExportMode; // Whether the filter is in export mode or not
-	bool m_bFirstErrorMessage; // Whether the first error message has been displayed or not
 
-    double totalWarpDuration; // The total duration of the current frame warp
-	double blendingScalar;
+    double m_dTotalWarpDuration; // The total duration of the current frame warp
+	double blendingScalar; // Blends from frame 1 to frame 2 (0.0 shows 100% frame 1, 1.0 shows 100% frame 2)
 };
